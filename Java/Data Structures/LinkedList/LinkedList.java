398aae4a3e714dd5de6417a50fe92ad7b2450ee3
AbGod <aboragodfred2@gmail.com>
2025-02-16 22:41:37 +0000

    single list in java

---
+++ Java/Data Structures/LinkedList/LinkedList.java
@@ -0,0 +1,35 @@
+public class LinkedList {
+    private Link first;
+
+    public LinkedList(){
+        first = null;
+    }
+
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    public void insertFirst(int id, double dd){
+        Link newLink = new Link(id, dd);
+        newLink.next = first;
+        first = newLink;
+    }
+
+    public Link deleteFirst() {
+        Link temp = first;
+        first = first.next;
+        return temp;
+    }
+
+    public void displayList(){
+        System.out.print("List (first-->last): ");
+        Link current = first;
+        while(current != null){
+            current.displayLink();
+            current = current.next;
+        }
+
+        System.out.println(" ");
+    }
+}
+
